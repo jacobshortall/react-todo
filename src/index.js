bf083d73d7332ce5c21986f2fe62614bac154f88
Jacob <Jacobshortall@gmail.com>
2022-06-25 15:35:43 +0100

    Add animation to clicked list elements

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import "./index.css";
 function ToDoList(props) {
     return props.items.map((item) => (
         <div className="row justify-content-center mb-3">
-            <div className="col-6 p-2 bg-light rounded-1">
+            <div
+                onClick={props.handleClick}
+                onAnimationEnd={props.handleAnimationEnd}
+                className="col-6 p-2 bg-light rounded-1 td-item"
+            >
                 <span className="fs-5">{item}</span>
             </div>
         </div>
@@ -16,8 +20,16 @@ class ToDoApp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            toDoItems: []
+            toDoItems: ["test", "testing"]
         };
+    }
+
+    handleClick(e) {
+        e.target.classList.add("clicked");
+    }
+
+    handleAnimationEnd(e) {
+        e.target.classList.remove("clicked");
     }
 
     render() {
@@ -25,11 +37,15 @@ class ToDoApp extends React.Component {
             <div className="container">
                 <div className="row justify-content-center mb-3 mt-2">
                     <div className="col-6 text-center">
-                        <h1 className="text-light">To-Do</h1>
+                        <h1 className="text-light fw-bold">To-Do</h1>
                     </div>
                 </div>
 
-                <ToDoList items={this.state.toDoItems} />
+                <ToDoList
+                    items={this.state.toDoItems}
+                    handleClick={this.handleClick}
+                    handleAnimationEnd={this.handleAnimationEnd}
+                />
             </div>
         );
     }
